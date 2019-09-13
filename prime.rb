c5992250b195b68a2a5be7ad7e90af9408981278
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-13 22:20:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+ if num <= 0 || num == 1 
+   false
+ else
+   collection = (2..num-1).to_a
+   collection.any? do |numero|
+     if numero % num == 0
+       true
+     else 
+       false
+     end
+   end
+ end
+end
