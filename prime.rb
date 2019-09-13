6ecf8cd53db139cf598b68316f2014993b8203a2
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-13 22:45:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+ if num <= 0 || num == 1 
+   false
+ else
+   collection = (2..num-1).to_a
+   collection.none? do |numero|
+     if num % numero == 0
+       true
+     else 
+       false
+     end
+   end
+ end
+end
